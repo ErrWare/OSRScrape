1e90a22e61c5239334e3b6bb1d55e36d0f03eafe
errware <ilanbuzzetti@gmail.com>
2018-09-03 08:03:12 -0500

    Added token dict class

--- myScrapingLib.py
+++ myScrapingLib.py
@@ -8,3 +8,23 @@ def getSoup(url):
 	except Exception as exc:
 		print('Problem requesting ' + url)
 	return null
+
+class TokenDict:
+	'Class doc string accessible via classname.__doc__'
+	myDict = {}
+	
+	def __init__(self):
+		myDict = {}
+
+	def getToken(key):
+		token = -1
+		if key in myDict:
+			token = myDict[key]['token']
+			myDict[key]['accesses'] += 1
+		else:
+			token = len(myDict)
+			myDict[key] = {}
+			myDict[key]['token'] = token
+			myDict[key]['accesses'] += 1
+		return token
+
