9181ad8fd560fca2fabf4150f9bbb05286648b13
errware <ilanbuzzetti@gmail.com>
2018-09-26 16:12:54 -0500

    now outputs json

--- armoryScrape.py
+++ armoryScrape.py
@@ -1,10 +1,11 @@
-# Scrape all equipment to new excel document
-# Outputs OSRS_ARMORY.xlsx
-import webbrowser
+# Scrape all equipment to new json dict
+# Outputs armory_v#.json
+
 import bs4
 import requests
-import openpyxl as xl
+import myScrapingLib as msl
 from myScrapingLib import getSoup
+import json
 
 def headCellName(headCell):
 	colRep = headCell.find('a')
@@ -23,7 +24,7 @@ def entryCellVal(entryCell):
 wikiNS = 'http://oldschoolrunescape.wikia.com'
 tableIndex = '/wiki/Category:Slot_tables'
 
-tableIndexSoup = getSoup(wikiNS+tableIndex) #bs4.BeautifulSoup(tableIndexPage.content, features='html5lib')
+tableIndexSoup = getSoup(wikiNS+tableIndex)
 
 titledLinks = tableIndexSoup.select('a[title]')
 slotTableLinks = [link for link in titledLinks if link.attrs['title'].endswith('slot table')]
@@ -42,52 +43,45 @@ for link in slotTableLinks:
 # Made into dict to remove dupes
 print('From		dict of table page uris')
 print('Making	dict of table heads and body elems')
-print('Making	excel db')
 
-WORKBOOK_NAME = 'OSRS_ARMORY'
-wb = xl.Workbook()
-# remove first sheet
-wb.remove(wb.worksheets[0])
+equipmentDict = {}
 
 for (key,value) in tableDict.items():
 	print(key + ' : ' + value)
 	tablePage = getSoup(value)
 	tableInPage = tablePage.select('table[class~=wikitable]')[0]
 	body = tableInPage.find('tbody')
+
+	
 	
 	# assume header is first row in tbody with no 'td'
 	head = body.find(lambda tag: len(tag.find_all('td')) == 0)
 	headColumns = head.find_all('th')
 	num_columns = len(headColumns)
+	
+	column_names = ['name']		# First th unlike rest
+	for th in headColumns:
+		try:
+			column_names.append(th.find('a').attrs['title'])
+		except:
+			pass
+	
 
 	# assume entries are rows in tbody with all 'td'
 	# size of each entry must match size of header
 	tableEntries = body.find_all(lambda tag: len(tag.find_all('td'))==num_columns)
 	print(len(tableEntries))
 
-	# Setup the excel sheet
-	SHEET_NAME = key
-	wb.create_sheet(SHEET_NAME)
-	ws = wb[SHEET_NAME]
-
-	ws['A1'] = value # store the uri to the page here
-	for row in ws.iter_rows(min_row=1, min_col=2, max_col=2+num_columns-1, max_row=1):
-		for cell in row:
-			cell.value = headCellName(headColumns.pop(0))
-	
-	for (index, entry) in enumerate(tableEntries):
-		print(index)
-		rowNum = index+2
+	for entry in tableEntries:
 		entryCells = entry.find_all('td')
 		#assume first entry cell has link out
-		ws.cell(row=rowNum, column=1).value = wikiNS + entryCells[0].find('a').attrs['href']
-		for colNum in range(2,num_columns+2):
-			print(colNum)
-			currCell = entryCells.pop(0)
-			cellVal = entryCellVal(currCell)
-			#print(cellVal)
-			ws.cell(row=rowNum, column=colNum).value = cellVal
+		print(entryCells[0].find('a').attrs['href'])
+		resourceToken = msl.rt_dict.getToken(entryCells[0].find('a').attrs['href'])
+		equipmentDict[resourceToken] = {}
 
+		for index, cell in enumerate(entryCells):
+			equipmentDict[resourceToken][column_names[index]] \
+				= cell.text.strip()
 
-
-wb.save(WORKBOOK_NAME+'.xlsx')
+with open('armory_v1.json','w') as armory_file:
+	armory_file.write(json.dumps(equipmentDict))
